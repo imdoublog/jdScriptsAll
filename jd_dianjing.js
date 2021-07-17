257ee8ed64b929a558e9be2d22d0c52c80deee71
徐鹏 <947760133@qq.com>
2021-07-17 22:28:01 +0800

    Update jd_dianjing.js

--- jd_dianjing.js
+++ jd_dianjing.js
@@ -10,7 +10,7 @@
  ==========================Quantumultx=========================
  [task_local]
  #东东电竞经理
- 0 0-23/2 * * * jd_EsportsManager.js, tag=东东电竞经理, img-url=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/icon/jd_EsportsManager.png, enabled=true
+ 0 2-23/2 * * * jd_EsportsManager.js, tag=东东电竞经理, img-url=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/icon/jd_EsportsManager.png, enabled=true
  =========================Loon=============================
  [Script]
  cron "0 0-23/2 * * *" script-path=jd_EsportsManager.js,tag=东东电竞经理
