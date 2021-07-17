c334c69aab1600376a05a5326335646cf450c663
徐鹏 <947760133@qq.com>
2021-07-18 00:06:00 +0800

    Update jd_jxmc.js

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -1,6 +1,6 @@
 /*
 京喜牧场
-15 15 * * *
+15 15 * * * jd_jxmc.js
 */
 // prettier-ignore
 !function (t, r) { "object" == typeof exports ? module.exports = exports = r() : "function" == typeof define && define.amd ? define([], r) : t.CryptoJS = r() }(this, function () {
