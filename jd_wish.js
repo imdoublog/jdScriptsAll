2c24cd6dc38d757dab7c8b657ff3354d5bdec77c
徐鹏 <947760133@qq.com>
2021-07-17 23:00:33 +0800

    Update jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -1,3 +1,4 @@
+// 0 13 * * *
 /*
 入口 京东众筹 许愿池 
 ====================
@@ -5,9 +6,7 @@
 TG通知群 https://t.me/ningmeng999
 微信公众号:柠檬玩机交流
 
-[task_local]
 #柠檬许愿池 
-// 0 8,12 * * * 
 */
 const $ = new Env('柠檬众筹许愿池');
 const notify = $.isNode() ? require("./sendNotify") : "";
