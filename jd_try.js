3fb6d306abbdd9054916babaf2d3117b12da4dbe
徐鹏 <947760133@qq.com>
2021-07-19 14:27:06 +0800

    Update jd_try.js

--- jd_try.js
+++ jd_try.js
@@ -26,8 +26,8 @@ const args = {
 	cidsList: ["家用电器", "手机数码", "电脑办公", "家居家装"],
 	typeList: ["普通试用", "闪电试用"],
 	goodFilters: "教程@软件@英语@辅导@培训".split('@'),
-	minPrice: 30,
-	maxSupplyCount: 30,
+	minPrice: 100, //最低价格
+	maxSupplyCount: 30, //最大数量
 }
 
 const cidsMap = {
